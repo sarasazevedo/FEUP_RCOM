ddc51d96c0f58c445cdef0b4b5abf502a9a49e7f
sarasazevedo <sarasazevedoo@gmail.com>
2024-10-05 14:54:41 +0100

    TP2 done

---
+++ TP2/write_noncanonical.c
@@ -0,0 +1,211 @@
+// Write to serial port in non-canonical mode
+//
+// Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+
+// Baudrate settings are defined in <asm/termbits.h>, which is
+// included by <termios.h>
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define FALSE 0
+#define TRUE 1
+
+#define BUF_SIZE 256
+#define MAX_RETRANSMISSIONS 3
+#define TIMEOUT 3
+
+volatile int STOP = FALSE;
+volatile int alarmEnabled = FALSE;
+volatile int alarmCount = 0;
+volatile int retransmissions = 0;
+
+#define FLAG 0x7E
+#define ADRESS_SENDER 0x03
+#define ADRESS_RECEIVER 0x01
+#define SET 0x03
+#define UA 0x07
+
+void memdump(void *addr, size_t bytes) {
+    for (size_t i = 0; i < bytes; ++i)
+        printf("%02x ", *((char*) addr + i));
+}
+
+// Alarm handler function to trigger retransmissions
+void alarmHandler(int signal) {
+    alarmEnabled = FALSE;
+    alarmCount++;
+    printf("Alarm #%d\n", alarmCount);
+
+    if (retransmissions < MAX_RETRANSMISSIONS) {
+        printf("Retransmitting SET frame...\n");
+        alarm(TIMEOUT);  // Restart the alarm
+        alarmEnabled = TRUE;
+        retransmissions++;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    // Program usage: Uses either COM1 or COM2
+    const char *serialPortName = argv[1];
+
+    if (argc < 2)
+    {
+        printf("Incorrect program usage\n"
+               "Usage: %s <SerialPort>\n"
+               "Example: %s /dev/ttyS1\n",
+               argv[0],
+               argv[0]);
+        exit(1);
+    }
+
+    // Open serial port device for reading and writing, and not as controlling tty
+    // because we don't want to get killed if linenoise sends CTRL-C.
+    int fd = open(serialPortName, O_RDWR | O_NOCTTY);
+
+    if (fd < 0)
+    {
+        perror(serialPortName);
+        exit(-1);
+    }
+
+    struct termios oldtio;
+    struct termios newtio;
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 10; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 0;  // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    // Set alarm function handler
+    signal(SIGALRM, alarmHandler);
+
+    // Create string to send
+    unsigned char buf[BUF_SIZE] = {0};
+
+    buf[0] = FLAG;
+    buf[1] = ADRESS_SENDER;
+    buf[2] = SET;
+    buf[3] = ADRESS_SENDER ^ SET;
+    buf[4] = FLAG;
+
+    unsigned char expected[5] = {FLAG, ADRESS_SENDER, UA, ADRESS_SENDER ^ UA, FLAG};
+
+    int bytes;
+
+    bytes = write(fd, buf, 5);
+    printf("%d bytes written (SET frame): ", bytes);
+    memdump(buf, 5);
+    printf("\n");
+
+    // Enable the alarm and retransmission logic
+    alarm(TIMEOUT);
+    alarmEnabled = TRUE;
+
+    while (retransmissions < MAX_RETRANSMISSIONS) {
+        printf("Waiting for UA frame...\n");
+        bytes = read(fd, buf, 5); // Attempt to read the UA frame
+
+        // Check if we received exactly 5 bytes (UA frame size)
+        if (bytes == 5) {
+            if (memcmp(buf, expected, 5) == 0) {
+                printf("UA frame received: ");
+                memdump(buf, 5);
+                printf("\n");
+                alarm(0); // Disable the alarm
+                break; // Exit loop upon successful reception of UA
+            } else {
+                printf("Received frame, but it's not UA. Ignoring...\n");
+            }
+        }
+
+        // Check if the alarm has sounded and we need to retransmit
+        if (!alarmEnabled) {
+            // Retransmit the SET frame
+            printf("Retransmitting SET frame...\n");
+            bytes = write(fd, buf, 5);
+            printf("%d bytes retransmitted (SET frame): ", bytes);
+            memdump(buf, 5);
+            printf("\n");
+
+            // Reset the alarm
+            alarm(TIMEOUT);
+            alarmEnabled = TRUE;
+        }
+
+        // Stop retransmissions if we've hit the limit
+        if (alarmCount >= MAX_RETRANSMISSIONS) {
+            printf("Maximum retransmissions reached, ending...\n");
+            break;
+        }
+    }
+
+    /*for (int i = 0; i < BUF_SIZE; i++)
+    {
+        buf[i] = 'a' + i % 26;
+    }*/
+
+    // In non-canonical mode, '\n' does not end the writing.
+    // Test this condition by placing a '\n' in the middle of the buffer.
+    // The whole buffer must be sent even with the '\n'.
+    /*buf[5] = '\n';
+
+    int bytes = write(fd, buf, BUF_SIZE);
+    printf("%d bytes written\n", bytes);*/
+
+    // Wait until all bytes have been written to the serial port
+    sleep(1);
+
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
+
+    return 0;
+}
